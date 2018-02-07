13af2bb5bd300a5592145e660bffa28285ff26c7
Khoa Pham <onmyway133@gmail.com>
2018-02-07 15:36:32 +0100

    Add RecyclerView

--- BasicSample/app/src/main/java/com/onmyway133/basicsample/ui/ProductAdapter.kt
+++ BasicSample/app/src/main/java/com/onmyway133/basicsample/ui/ProductAdapter.kt
@@ -1,7 +1,22 @@
 package com.onmyway133.basicsample.ui
 
-/**
- * Created by khoa on 07.02.2018.
- */
-class ProductAdapter {
+import android.support.v7.widget.RecyclerView
+import android.view.ViewGroup
+
+class ProductAdapter: RecyclerView.Adapter<ProductAdapter.ProductViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): ProductViewHolder {
+        TODO("not implemented")
+    }
+
+    override fun onBindViewHolder(holder: ProductViewHolder, position: Int) {
+        TODO("not implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented")
+    }
+
+    class ProductViewHolder: RecyclerView.ViewHolder() {
+
+    }
 }
